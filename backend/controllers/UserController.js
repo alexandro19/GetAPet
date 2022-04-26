c975bcb94bd4a3245a7c393536a755f860a5ca04
Alexandro <ale_go121@hotmail.com>
2022-04-26 17:07:02 -0300

    declaracao de variaveis name e email na UserController

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,8 @@ const User = require('../models/User')
 
 module.exports = class UserController{
   static async register(req, res){
+    const name = req.body.name
+    const email = req.body.email
     res.json('Olá Get A Pet')
   }  
 }
