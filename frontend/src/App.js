439c863cab4a155802beb351df041afb09f8eb7e
Alexandro <ale_go121@hotmail.com>
2022-05-01 11:34:35 -0300

    Estrutuando o frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+//Components
+import NavBar from "./components/layout/NavBar"
+import Footer from "./components/layout/Footer"
+import Container from "./components/layout/Container"
+
+//Pages
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
+import Home from './components/pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar/>
+      <Container>
+        <Routes>
+          <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<Register/>} />
+          <Route path='/' element={<Home/>} />
+        </Routes>
+      </Container>
+      <Footer/>
+    </Router> 
   );
 }
 
---
+++ frontend/src/components/pages/Auth/Register.js
@@ -0,0 +1,12 @@
+function Register() {
+    return (
+      <section>
+        <h1>Registrar</h1>
+        <form action="">
+          
+        </form>
+      </section>      
+    )    
+  }
+  
+  export default Register
