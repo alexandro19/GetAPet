818239c740173663e8e5a35e56c69c56cfff657f
Alexandro <ale_go121@hotmail.com>
2022-05-02 17:53:29 -0300

    Finalizando a parte de cadastro de usuário

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,24 +4,31 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from "./components/layout/NavBar"
 import Footer from "./components/layout/Footer"
 import Container from "./components/layout/Container"
+import Message from './components/layout/Message'
 
 //Pages
 import Login from './components/pages/Auth/Login'
 import Register from './components/pages/Auth/Register'
 import Home from './components/pages/Home'
 
+//Contexts
+import { UserProvider } from './context/UserContext'
+
 function App() {
   return (
     <Router>
-      <NavBar/>
-      <Container>
-        <Routes>
-          <Route path='/login' element={<Login/>} />
-          <Route path='/register' element={<Register/>} />
-          <Route path='/' element={<Home/>} />
-        </Routes>
-      </Container>
-      <Footer/>
+      <UserProvider>
+        <NavBar/>
+        <Message/>
+        <Container>
+          <Routes>
+            <Route path='/login' element={<Login/>} />
+            <Route path='/register' element={<Register/>} />
+            <Route path='/' element={<Home/>} />
+          </Routes>
+        </Container>
+        <Footer/>
+      </UserProvider>
     </Router> 
   );
 }
